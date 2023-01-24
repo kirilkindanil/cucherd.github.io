129fb84d2df92461d749840d7c5f521898c0ffda
a123 <kirilkingames@gmail.com>
2023-01-24 20:52:37 +0700

    update

--- testbot/main.py
+++ testbot/main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher, executor
 loop = asyncio.new_event_loop()
-bot = Bot(token='5813307847:AAHtB8yiu3NXC88CjKeql03g83TZu81pim8', parse_mode='HTML')
+bot = Bot(token='5642318179:AAHRSLNE7A0fkHYduS_XiRsSQ4EFh2U-A6w', parse_mode='HTML')
 dp = Dispatcher(bot, loop)
 
 if __name__ == '__main__':
